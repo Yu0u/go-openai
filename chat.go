8460ea40bbe3f8261d336141edb39ecc0e9b60f2
耿立夏 <genglixia@enjoypartytime.com>
2025-05-20 10:52:27 +0800

    feat(chat): support qwen3 thinking mode

--- chat.go
+++ chat.go
@@ -280,6 +280,9 @@ type ChatCompletionRequest struct {
 	// Such as think mode for qwen3. "chat_template_kwargs": {"enable_thinking": false}
 	// https://qwen.readthedocs.io/en/latest/deployment/vllm.html#thinking-non-thinking-modes
 	ChatTemplateKwargs map[string]any `json:"chat_template_kwargs,omitempty"`
+	
+	EnableThinking     bool           `json:"enable_thinking,omitempty"`
+	ThinkingBudget     int            `json:"thinking_budget,omitempty"`
 }
 
 type StreamOptions struct {
